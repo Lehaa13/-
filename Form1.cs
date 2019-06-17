85039baaab30b464127e99e37c5a9561692fe9a2
Lehaa13 <47592474+Lehaa13@users.noreply.github.com>
2019-06-17 16:46:30 +0700

    Заготовка

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pppp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button13_Click_1(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+
+            button(position).Text = position.ToString();
+        }
+        private Button button (int position)
+        {
+            switch (position)
+            {
+                case 0: return button0;
+                case 1: return button1;
+                case 2: return button2;
+                case 3: return button3;
+                case 4: return button4;
+                case 5: return button5;
+                case 6: return button6;
+                case 7: return button7;
+                case 8: return button8;
+                case 9: return button8;
+                case 10: return button10;
+                case 11: return button11;
+                case 12: return button12;
+                case 13: return button13;
+                case 14: return button14;
+                case 15: return button15;
+                default: return null;
+            }
+
+        }
+    }
+
+}
